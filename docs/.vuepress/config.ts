1f70a9d9e9cfdf673756ded7a5365b67d2ea67cb
wyl <wuyulin@chucp.mobi>
2025-05-09 19:02:49 +0800

    feat: 创建blog项目

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,41 @@
+export default {
+  title: 'xx合集',
+  description: 'xx合集展示',
+  theme: 'reco',
+  base: '/blog-view/',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '宇霖的 web 博客',
+        items: [{ text: 'Github', link: 'https://github.com/velin666' }],
+      },
+    ],
+    logo: '/assets/img/logo.png',
+    sidebar: [
+      {
+        title: '首页',
+        path: '/',
+        collapsable: false, // 不折叠
+      },
+      {
+        title: 'demo示例',
+        path: '/DEMO',
+        collapsable: false, // 不折叠
+      },
+    ],
+    subSidebar: 'auto',
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': './',
+      },
+    },
+  },
+}
---
+++ docs/DEMO.md
@@ -0,0 +1,33 @@
+---
+title: 主标题
+author: 宇霖
+---
+
+markdowm教程 <https://markdown.com.cn/basic-syntax/links.html>
+表情包 <https://gist.github.com/rxaviers/7360908>
+
+# h1
+## h2
+### h3
+
+这是强调内容**sss**
+
+~~去掉的内容~~
+
+> 哈哈哈哈 :joy:
+>> 未知
+
+```html
+<p>ddd</p>
+<div>5555</div>
+```
+---
+```json
+{
+  "type": "modules"
+  "methods": {
+    "title": "这是标题",
+    "children": []
+  }
+}
+```
